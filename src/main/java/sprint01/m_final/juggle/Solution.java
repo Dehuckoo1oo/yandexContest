b757b4ecc4b43ccb74a861fd6775885f2e25044f
dehuckoo1oo <dehuckoo1oo@mail.ru>
2026-04-14 10:40:06 +0300

    s1 11/12 + final 2/2 refactoring

--- src/main/java/sprint01/m_final/juggle/Solution.java
+++ src/main/java/sprint01/m_final/juggle/Solution.java
@@ -3,25 +3,25 @@ package sprint01.m_final.juggle;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//Ссылка на отчет: https://contest.yandex.ru/contest/22450/run-report/160300441/
+//Ссылка на отчет: https://contest.yandex.ru/contest/22450/run-report/160379852/
 class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int[] countersByNumbers = new int[9];
         int k = Integer.parseInt(reader.readLine());
         int count = 0;
-        for (int i = 0; i < 4; i++) {
+        for (int row = 0; row < 4; row++) {
             String str = reader.readLine();
-            for (int j = 0; j < 4; j++) {
-                char symbol = str.charAt(j);
+            for (int elem = 0; elem < 4; elem++) {
+                char symbol = str.charAt(elem);
                 if (symbol != '.') {
                     int number = Character.getNumericValue(symbol);
                     countersByNumbers[number - 1] = countersByNumbers[number - 1] + 1;
                 }
             }
         }
-        for (int i = 0; i <= 8; i++) {
-            if (countersByNumbers[i] <= k * 2 && countersByNumbers[i] > 0) {
+        for (int number = 0; number <= 8; number++) {
+            if (countersByNumbers[number] <= k * 2 && countersByNumbers[number] > 0) {
                 count++;
             }
         }
--- src/main/java/sprint01/m_final/nearestZero/Solution.java
+++ src/main/java/sprint01/m_final/nearestZero/Solution.java
@@ -4,28 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-//Ссылка на отчет: https://contest.yandex.ru/contest/22450/run-report/160297161/
+//Ссылка на отчет: https://contest.yandex.ru/contest/22450/run-report/160379779/
 class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(reader.readLine());
         StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        int[] strArray = new int[count];
+        int[] houseNumberArray = new int[count];
         for (int i = 0; i < count; i++) {
-            strArray[i] = Integer.parseInt(tokenizer.nextToken());
+            houseNumberArray[i] = Integer.parseInt(tokenizer.nextToken());
         }
         int[] rightArray = new int[count];
         StringBuilder sb = new StringBuilder();
         int lastZero = Integer.MAX_VALUE;
         for (int i = count - 1; i > -1; i--) {
-            if (strArray[i] == 0) {
+            if (houseNumberArray[i] == 0) {
                 lastZero = i;
             }
             rightArray[i] = lastZero - i;
         }
+        /*Такое значение нужно т.к. с Integer.MIN_VALUE возникает проблема с переполнением, если 0 еще не встречался,
+        например 0 - (-2147483648) = -2147483648;
+        пример в отчете: https://contest.yandex.ru/contest/22450/run-report/160379131/
+         */
         lastZero = -2_000_001;
         for (int i = 0; i < count; i++) {
-            if (strArray[i] == 0) {
+            if (houseNumberArray[i] == 0) {
                 lastZero = i;
             }
             sb.append(Math.min(rightArray[i], i - lastZero)).append(" ");
